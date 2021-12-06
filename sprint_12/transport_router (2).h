b37fda82d4538f4d171f788e87275c4b58db22a3
Stas Lyutsko <staslutzko@mail.ru>
2021-12-06 13:00:47 +0300

    */*/*//

---
+++ sprint_12/transport_router (2).h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "transport_catalogue.h"
+#include "domain.h"
+#include "router.h"
+
+namespace transport_router
+{
+	class TransportRouter
+	{
+	public:
+
+		TransportRouter(const transport_catalogue::TransportCatalogue& trans_cat);
+
+		void SetRoutingSettings(const int bus_wait_time, const double bus_velocity);
+
+		std::optional<graph::Router<double>::RouteInfo> GetRoute(const std::string_view from, const std::string_view to);
+
+	private:
+
+		const transport_catalogue::TransportCatalogue& trans_cat_;
+		domain::RoutingSettings settings_;
+		std::unique_ptr<graph::Router<double>> router_ = nullptr;
+		graph::DirectedWeightedGraph<double> graph_;
+
+		void FillGraph();
+	};
+} // namespace transport_router
