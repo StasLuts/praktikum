ec29cc1a77bc954a63d5006ac29aecd759a47bb9
Stas Lyutsko <staslutzko@mail.ru>
2022-03-29 12:34:36 +0300

    /*/*/*/

---
+++ graduate_work/tree/include/cell.cpp
@@ -0,0 +1,38 @@
+﻿#include "cell.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <optional>
+
+
+// Реализуйте следующие методы
+Cell::Cell()
+{
+
+}
+
+Cell::~Cell()
+{
+
+}
+
+void Cell::Set(std::string text)
+{
+
+}
+
+void Cell::Clear()
+{
+
+}
+
+Cell::Value Cell::GetValue() const
+{
+	return "";
+}
+
+std::string Cell::GetText() const
+{
+	return "";
+}
